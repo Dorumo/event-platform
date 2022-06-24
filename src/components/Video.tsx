e7b6c8d30b5e7d3ba87fe31220269d1564020c57
Artur Moraes <artur.gomoraes@gmail.com>
2022-06-23 22:02:23 -0300

    controle de subscribe

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,6 +4,7 @@ import {
   Lightning,
   FileArrowDown,
   CaretRight,
+  Image
 } from "phosphor-react";
 
 import "@vime/core/themes/default.css";
@@ -133,7 +134,7 @@ export function Video(props: VideoProps) {
             className="bg-gray-700 rounded overflow-hidden flex items-strech gap-6 hover:bg-gray-600 transition-colors"
           >
             <div className="bg-green-700 h-full p-6 flex items-center">
-              <FileArrowDown size={40} />
+              <Image size={40} />
             </div>
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">Wallpapers exclusivos</strong>
